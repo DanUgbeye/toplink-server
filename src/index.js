4e20e8bfba79b6b6fb644cdb49b78aa8947234c7
Daniel Ugbeye <ugbeyellionz@gmail.com>
2022-04-19 21:22:10 +0100

    tuts

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-app.use(cors);
-app.use(morgan('combined'));
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.route('/').get((req, res) => {
+    res.send('server up and running!!!');
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
