b53d475735a202a313a0ec83038da77d2e51f705
Nickspizza001 <dbodun56@gmail.com>
2022-04-23 04:42:59 +0100

    dami

--- src/modules/links/models.js
+++ src/modules/links/models.js
@@ -1,1 +1,75 @@
 // all the models come in here
+const linkSchema = require("./schema");
+const mongoose = require('mongoose')
+
+const linkModel = mongoose.model('link', linkSchema);
+
+
+
+async function createNewLink() {
+  let linkTitle = req.body.linkTitle;
+  let linkUrl = req.body.linkUrl;
+  function check(linkTitle, linkUrl) {
+    let linkTitleDuplicate = linkModel.find((link) => link.title == linkTitle);
+    let linkUrlDuplicate = linkModel.find((link) => link.Url == linkUrl);
+    if (!linkUrlDuplicate || !linkTitleDuplicate) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  if (check == 1) {
+    try {
+      const newLink = new linkModel({
+        id,
+        author,
+        title,
+        description,
+        url,
+        icon,
+        privacy,
+      });
+
+      const result = await newLink.save();
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("link title or link url exists already");
+  }
+}
+
+async function updateLink(id) {
+  const id = id;
+  const findId = linkModel.find((link) => link.id == id);
+  if (!findId) {
+    console.log("Error: Link doesnt exist");
+  } else {
+    try {
+      let updatedLink = {
+        author,
+        title,
+        description,
+        url,
+        icon,
+        privacy,
+      };
+      const result = await linkModel.findByIdAndUpdate(id, updatedLink);
+      return result;
+    } catch (error) {}
+  }
+}
+
+async function deleteLink(id) {
+  await linkModel
+    .findByIdAndDelete(id)
+    .then((res) => console.log("Deleted!"))
+    .catch((err) => console.log(err));
+}
+
+async function getAllLink(author) {
+  const result = await linkModel.find({ author: author });
+  return result;
+}
--- src/modules/users/models.js
+++ src/modules/users/models.js
@@ -1,1 +1,87 @@
 // all the models come in here
+const userSchema = require("./schema");
+const mongoose = require('mongoose');
+const userModel = mongoose.model('User', userSchema);
+
+
+async function createNewUser() {
+  
+  function check(req){
+    let userName = req.body.username;
+    let userEmail = req.body.email;
+    
+    let phoneNumber = req.body.phoneNumber
+    let userNameDuplicate = userModel.find((user) => user.username == userName);
+    let userEmailDuplicate = userModel.find((user) => user.email == userEmail);
+    let userPhoneNumberDuplicate = userModel.find((user)=> user.phoneNumber == phoneNumber)
+    if(!userEmailDuplicate || !userPhoneNumberDuplicate || !userNameDuplicate){
+      return true
+    }
+    
+  }
+ 
+  if (check == true) {
+    try {
+      const newUser = new userModel({
+        id,
+        Name,
+        username,
+        email,
+        phoneNumber,
+        profilePhoto,
+        bio,
+        role,
+        coverPhoto,
+        subscription,
+        disabled,
+        privacy,
+      });
+
+      const result = await newUser.save();
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("Email  address or Phone number or Username already in use");
+  }
+}
+
+async function updateUser(id) {
+  const id = id;
+  const findId = userModel.find((user) => user.id == id);
+  if (!findId) {
+    console.log("Error: User doesnt exist");
+  } else {
+    try {
+      let updatedUser = {
+        Name,
+        username,
+        email,
+        phoneNumber,
+        profilePhoto,
+        bio,
+        role,
+        coverPhoto,
+        subscription,
+        disabled,
+        privacy,
+      };
+      const result = await userModel.findByIdAndUpdate(id, updatedUser);
+      return result;
+    } catch (error) {}
+  }
+}
+
+async function deleteUser(id) {
+  await userModel
+    .findByIdAndDelete(id)
+    .then((res) => console.log("Deleted!"))
+    .catch((err) => console.log(err));
+}
+
+async function getAllUsers() {
+  const result = await userModel.find();
+  return result;
+}
+
