fcea6c06956c865257d1f691c056f1b9173e690e
Nickspizza001 <dbodun56@gmail.com>
2022-04-26 04:46:47 +0100

    link model

--- src/modules/links/models.js
+++ src/modules/links/models.js
@@ -1,70 +1,101 @@
+
 // all the models come in here
+<<<<<<< Updated upstream
 const linkModel = require("./schema");
+=======
+const mongoose = require("mongoose");
+const { linkSchemaValidator, linkSchema } = require("./schema");
+const { validateData } = require("../../utils/validator");
+const Response = require("../../utils/response");
 
-async function createNewLink() {
-  let linkTitle = req.body.linkTitle;
-  let linkUrl = req.body.linkUrl;
-  function check(linkTitle, linkUrl) {
-    let linkTitleDuplicate = linkModel.find((link) => link.title == linkTitle);
-    let linkUrlDuplicate = linkModel.find((link) => link.Url == linkUrl);
-    if (!linkUrlDuplicate || !linkTitleDuplicate) {
-      return 1;
-    } else {
-      return 0;
+const linkModel = mongoose.model("Link", linkSchema);
+
+async function isUniqueLink(link, author) {
+  let link = await linkModel.find({
+    $and: [{ author: author }, { link: link }],
+  });
+  if (!link) {
+    return true;
+  }
+  return false;
+}
+
+exports.getLLinkById = async function getLinkById(id) {
+  const result = await linkModel.find({ _id: id });
+  if (!result) {
+    return Response("error", 400, "invalid link id provided");
+  }
+  return Response("success", 201, "fetched link successfully", result);
+};
+>>>>>>> Stashed changes
+
+exports.createNewLink = async function createNewLink(link) {
+  let validatedData = validateData(link, linkSchemaValidator);
+
+  // if the link data is not valid, return an error response
+  if (!validatedData.isValid) {
+    return Response("error", 400, validatedData.error);
+  }
+
+  if ((!isUniqueLink(link.url), link.author)) {
+    return Response("error", 400, " Link already exists");
+  }
+
+  const newLink = new linkModel(link);
+  newLink
+    .save()
+    .then((result) => {
+      return Response("success", 201, "Link created successfully", result);
+    })
+    .catch((error) => {
+      return Response("error", 500, error);
+    });
+};
+
+exports.updateLink = async function updateLink(id, linkData) {
+  if (linkData.url) {
+    if (!isUniqueLink(linkData.url, linkData.author)) {
+      return Response(
+        "error",
+        400,
+        "This link already exists with this author "
+      );
     }
   }
 
-  if (check == 1) {
-    try {
-      const newLink = new linkModel({
-        id,
-        author,
-        title,
-        description,
-        url,
-        icon,
-        privacy,
-      });
-
-      const result = await newLink.save();
-      return result;
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    console.log("link title or link url exists already");
+  const link = linkModel.find({ _id: id });
+  if (!link) {
+    return Response("error", 400, "invalid link id provided");
   }
-}
-
-async function updateLink(id) {
-  const id = id;
-  const findId = linkModel.find((link) => link.id == id);
-  if (!findId) {
-    console.log("Error: Link doesnt exist");
-  } else {
-    try {
-      let updatedLink = {
-        author,
-        title,
-        description,
-        url,
-        icon,
-        privacy,
-      };
-      const result = await linkModel.findByIdAndUpdate(id, updatedLink);
-      return result;
-    } catch (error) {}
+  try {
+    // updates the link data
+    const updatedLink = {
+      ...link,
+      ...linkData,
+    };
+    const result = await linkModel.findByIdAndUpdate(id, updatedLink);
+    return Response("success", 201, "Link updated successfully", result);
+  } catch (error) {
+    return Response("error", 500, error);
   }
-}
+};
 
-async function deleteLink(id) {
-  await linkModel
-    .findByIdAndDelete(id)
-    .then((res) => console.log("Deleted!"))
-    .catch((err) => console.log(err));
-}
+exports.deleteLink = async function deleteLink(id) {
+  const deletedLink = await linkModel.findByIdAndDelete(id);
+  if (!deletedLink) {
+    return Response("error", 400, "invalid link id provided");
+  }
+  return Response("success", 200, "link deleted successfully");
+};
 
-async function getAllLink(author) {
-  const result = await linkModel.find({ author: author });
-  return result;
-}
+exports.getAllLinks = async function getAllLink(author) {
+  const result = await linkModel.find({
+    author: author,
+  });
+  return Response(
+    "success",
+    201,
+    "fetched all links relating to this author successfully",
+    result
+  );
+};
