0a328329a8cc41188a0878375f702e8b78a4a698
Daniel Ugbeye <ugbeyellionz@gmail.com>
2022-04-27 12:53:39 +0100

    added express.json middleware

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -3,12 +3,12 @@ const userRouter = require('express').Router()
 const { getUserById, createNewUser, updateUser, deleteUser } = require("./controllers");
 
 
-userRouter.route('/user').post(createNewUser)
+userRouter.route('/').post(createNewUser)
 
-userRouter.route('/user/:id').get(getUserById)
+userRouter.route('/:id').get(getUserById)
 
-userRouter.route('/user/:id').patch(updateUser)
+userRouter.route('/:id').patch(updateUser)
 
-userRouter.route('/user/:id').delete(deleteUser)
+userRouter.route('/:id').delete(deleteUser)
 
 module.exports = { userRouter }
