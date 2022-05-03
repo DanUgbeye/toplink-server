cb1e72ce6c7d30d7123a5c54e9a9035fb3b0e6cd
Marvelous Akporowho <solomonmarvel@hotmail.com>
2022-05-03 21:27:10 +0100

    Update controllers.js
    added comments here as a pointer on what to do (move the validation codes to the controller files)

--- src/modules/links/controllers.js
+++ src/modules/links/controllers.js
@@ -2,18 +2,21 @@
 const { getLinkById, createLink, updateLink, deleteLink } = require("./models");
 
 exports.createLink = async (req, res) => {
+    // validate here
     let link = req.body;
     const response = await createLink(link);
     res.status(response.code).send(response);
 }
 
 exports.getLinkById = async (req, res) => {
+    // validate here
     let id = req.params.id
     const response = await getLinkById(id);
     res.status(response.code).send(response);
 }
 
 exports.updateLink = async (req, res) => {
+    // validate here
     let id = req.params.id;
     const linkData = req.body;
     const response = await updateLink(id, linkData)
@@ -21,6 +24,7 @@ exports.updateLink = async (req, res) => {
 }
 
 exports.deleteLink = async (req, res) => {
+    // validate here
     let id = req.params.id;
     const response = await deleteLink(id);
     res.status(response.code).send(response);
